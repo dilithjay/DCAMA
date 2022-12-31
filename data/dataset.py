24c7bc4204abfb6134196a72d0b0e27d105d8429
dilithjay <dilithjay@gmail.com>
2022-12-31 23:47:17 +0530

    Update serp training and dataset

--- data/dataset.py
+++ data/dataset.py
@@ -9,59 +9,40 @@ from data.serp import DatasetSerp
 
 
 class FSSDataset:
-
     @classmethod
     def initialize(cls, img_size, datapath, use_original_imgsize):
 
         cls.datasets = {
-            'pascal': DatasetPASCAL,
-            'coco': DatasetCOCO,
-            'fss': DatasetFSS,
-            'serp': DatasetSerp,
+            "pascal": DatasetPASCAL,
+            "coco": DatasetCOCO,
+            "fss": DatasetFSS,
+            "serp": DatasetSerp,
         }
 
-        cls.img_mean = [                                                                                
-            0.12375696117681859,                                                         
-            0.1092774636368323,                                                          
-            0.1010855203267882,                                                          
-            0.1142398616114001,                                                          
-            0.1592656692023089,                                                          
-            0.18147236008771792,                                                         
-            0.1745740312291377,                                                          
-            0.19501607349635292,                                                         
-            0.15428468872076637,                                                         
-            0.10905050699570007,                                                         
-        ]
-        cls.img_std = [                                                                                
-            0.03958795985905458,                                                         
-            0.047778262752410296,                                                        
-            0.06636616706371974,                                                         
-            0.06358874912497474,                                                         
-            0.07744387147984592,                                                         
-            0.09101635085921553,                                                         
-            0.09218466562387101,                                                         
-            0.10164581233948201,                                                         
-            0.09991773043519253,                                                         
-            0.08780632509122865,                                                         
-        ]
+        cls.img_mean = [455, 675, 400, 1000, 2480, 2905, 3040, 3130, 1810, 950]
+
+        cls.img_std = [185, 148, 99, 225, 465, 557, 625, 594, 412, 306]
         cls.datapath = datapath
         cls.use_original_imgsize = use_original_imgsize
-        
-        cls.transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=cls.img_mean, std=cls.img_std)
-        ])
+
+        cls.transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize(mean=cls.img_mean, std=cls.img_std)]
+        )
 
     @classmethod
     def build_dataloader(cls, benchmark, bsz, nworker, fold, split, shot=1):
-        nworker = nworker if split == 'trn' else 0
+        nworker = nworker if split == "trn" else 0
 
-        dataset = cls.datasets[benchmark](cls.datapath, fold=fold,
-                                          transform=cls.transform,
-                                          split=split, shot=shot, use_original_imgsize=cls.use_original_imgsize)
+        dataset = cls.datasets[benchmark](
+            cls.datapath,
+            fold=fold,
+            transform=cls.transform,
+            split=split,
+            shot=shot,
+            use_original_imgsize=cls.use_original_imgsize,
+        )
         # Force randomness during training for diverse episode combinations
         # Freeze randomness during testing for reproducibility
-        dataloader = DataLoader(dataset, batch_size=bsz, shuffle=False, num_workers=nworker,
-                                pin_memory=True)
+        dataloader = DataLoader(dataset, batch_size=bsz, shuffle=False, num_workers=nworker, pin_memory=True)
 
         return dataloader
--- data/serp.py
+++ data/serp.py
@@ -11,49 +11,64 @@ import numpy as np
 
 class DatasetSerp(Dataset):
     def __init__(self, datapath, fold, transform, split, shot, use_original_imgsize):
-        self.split = 'val' if split in ['val', 'test'] else 'trn'
+        self.split = "val" if split in ["val", "test"] else "trn"
         self.fold = fold
         self.nfolds = 4
         self.nclass = 1
-        self.benchmark = 'serp'
+        self.benchmark = "serp"
         self.shot = shot
-        self.split_coco = split if split == 'val' else 'train'
-        self.base_path = os.path.join(datapath, 'Serp', str(fold))
+        self.split_coco = split if split == "val" else "train"
+        self.base_path = os.path.join(datapath, "Serp", str(fold))
         self.transform = transform
         self.use_original_imgsize = use_original_imgsize
+        self.count = 0
 
         self.class_ids = [0]
         self.img_metadata_classwise = self.build_img_metadata_classwise()
         self.img_metadata = self.build_img_metadata()
 
     def __len__(self):
-        return len(self.img_metadata) if self.split == 'trn' else 8
+        return len(self.img_metadata)
 
     def __getitem__(self, idx):
         # ignores idx during training & testing and perform uniform sampling over object classes to form an episode
         # (due to the large size of the COCO dataset)
-        query_img, query_mask, support_imgs, support_masks, query_name, support_names, class_sample, org_qry_imsize = self.load_frame()
+        (
+            query_img,
+            query_mask,
+            support_imgs,
+            support_masks,
+            query_name,
+            support_names,
+            class_sample,
+            org_qry_imsize,
+        ) = self.load_frame()
 
         query_img = self.transform(query_img)
+        # print(query_img.max(), query_img.min(), query_img.mean(), query_img.std())
         query_mask = query_mask.float()
         if not self.use_original_imgsize:
-            query_mask = F.interpolate(query_mask.unsqueeze(0).unsqueeze(0).float(), query_img.size()[-2:], mode='nearest').squeeze()
+            query_mask = F.interpolate(
+                query_mask.unsqueeze(0).unsqueeze(0).float(), query_img.size()[-2:], mode="nearest"
+            ).squeeze()
 
         support_imgs = torch.stack([self.transform(support_img) for support_img in support_imgs])
         for midx, smask in enumerate(support_masks):
-            support_masks[midx] = F.interpolate(smask.unsqueeze(0).unsqueeze(0).float(), support_imgs.size()[-2:], mode='nearest').squeeze()
+            support_masks[midx] = F.interpolate(
+                smask.unsqueeze(0).unsqueeze(0).float(), support_imgs.size()[-2:], mode="nearest"
+            ).squeeze()
         support_masks = torch.stack(support_masks)
 
-        batch = {'query_img': query_img.float(),
-                 'query_mask': query_mask.float(),
-                 'query_name': query_name,
-
-                 'org_query_imsize': org_qry_imsize,
-
-                 'support_imgs': support_imgs.float(),
-                 'support_masks': support_masks.float(),
-                 'support_names': support_names,
-                 'class_id': torch.tensor(class_sample)}
+        batch = {
+            "query_img": query_img.float(),
+            "query_mask": query_mask.float(),
+            "query_name": query_name,
+            "org_query_imsize": org_qry_imsize,
+            "support_imgs": support_imgs.float(),
+            "support_masks": support_masks.float(),
+            "support_names": support_names,
+            "class_id": torch.tensor(class_sample),
+        }
 
         return batch
 
@@ -61,13 +76,15 @@ class DatasetSerp(Dataset):
         nclass_trn = self.nclass // self.nfolds
         class_ids_val = [self.fold + self.nfolds * v for v in range(nclass_trn)]
         class_ids_trn = [x for x in range(self.nclass) if x not in class_ids_val]
-        class_ids = class_ids_trn if self.split == 'trn' else class_ids_val
+        class_ids = class_ids_trn if self.split == "trn" else class_ids_val
 
         return class_ids
 
     def build_img_metadata_classwise(self):
-        train_imgs = glob(os.path.join(self.base_path, self.split_coco, '*.npy'))
-        img_metadata_classwise = {0: list(map(lambda x: os.path.relpath(x, os.path.dirname(os.path.dirname(x))), train_imgs))}
+        train_imgs = glob(os.path.join(self.base_path, self.split_coco, "*.npy"))
+        img_metadata_classwise = {
+            0: list(map(lambda x: os.path.relpath(x, os.path.dirname(os.path.dirname(x))), train_imgs))
+        }
         return img_metadata_classwise
 
     def build_img_metadata(self):
@@ -77,38 +94,47 @@ class DatasetSerp(Dataset):
         return sorted(list(set(img_metadata)))
 
     def read_mask(self, name):
-        mask_path = os.path.join(self.base_path, 'annotations', name)
-        mask = torch.tensor(np.array(Image.open(mask_path[:mask_path.index('.npy')] + '-mask.png'))) / 255
+        mask_path = os.path.join(self.base_path, "annotations", name)
+        mask = torch.tensor(np.array(Image.open(mask_path[: mask_path.index(".npy")] + "-mask.png"))) / 255
         return mask
 
     def load_frame(self):
         class_sample = np.random.choice(self.class_ids, 1, replace=False)[0]
         query_name = np.random.choice(self.img_metadata_classwise[class_sample], 1, replace=False)[0]
         query_img = np.load(os.path.join(self.base_path, query_name)).astype(float).transpose(1, 2, 0)
-        query_img /= 2000  # = (query_img - 1800) / 1300
         query_mask = self.read_mask(query_name)
 
         org_qry_imsize = query_img.shape
-        
+
         query_mask[query_mask != class_sample + 1] = 0
         query_mask[query_mask == class_sample + 1] = 1
 
         support_names = set()
         while True:  # keep sampling support set if query == support
             support_name = np.random.choice(self.img_metadata_classwise[class_sample], 1, replace=False)[0]
-            if query_name != support_name: support_names.add(support_name)
-            if len(support_names) == self.shot: break
+            if query_name != support_name:
+                support_names.add(support_name)
+            if len(support_names) == self.shot:
+                break
 
         support_names = list(support_names)
         support_imgs = []
         support_masks = []
         for support_name in support_names:
-            support_img = np.load(os.path.join(self.base_path, support_name)).astype(float)[:10, :, :].transpose(1, 2, 0)
-            support_imgs.append(support_img / 2000)
+            support_img = np.load(os.path.join(self.base_path, support_name)).astype(float).transpose(1, 2, 0)
+            support_imgs.append(support_img)
             support_mask = self.read_mask(support_name)
             support_mask[support_mask != class_sample + 1] = 0
             support_mask[support_mask == class_sample + 1] = 1
             support_masks.append(support_mask)
-        
-        return query_img, query_mask, support_imgs, support_masks, query_name, support_names, class_sample, org_qry_imsize
 
+        return (
+            query_img,
+            query_mask,
+            support_imgs,
+            support_masks,
+            query_name,
+            support_names,
+            class_sample,
+            org_qry_imsize,
+        )
